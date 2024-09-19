21ac7108500a0b45d1f17d0797492f3c495b384e
Marius Skjold Bjerg <marius.bjerg@gmail.com>
2024-09-19 22:03:32 +0200

    Added import file

---
+++ frontend/carhub/src/imports.ts
@@ -0,0 +1,9 @@
+/**
+ * @fileoverview imports.ts is a file that contains all the imports for the frontend.
+ * @author Marius Skjold Bjeg (msb)
+ */
+
+// Hooks
+import { useEffect, useState } from "react";
+
+export { useEffect, useState };
